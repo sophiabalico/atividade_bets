a36e020b3d2a04decece75200ee2353cbb9e4ccc
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-10-10 15:00:29 -0300

    feat: criação do servidor

---
+++ src/routes/index.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const routes = Router();
+
+// Rota raiz para teste
+routes.get("/", (req, res) => {
+  return res.status(200).json({ message: "Vai Corinthians!" });
+});
+
+export default routes;
---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const serverPort = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello, World!" });
+});
+
+app.listen(serverPort, () => {
+  console.log(`⚡ Server started on http://localhost:${serverPort}`);
+});
